e273f8c15cc1cd4577a810deea8117a7dda48ea2
Wietse Venema <wietsevenema@gmail.com>
2020-02-25 21:27:55 +0100

    Add HTTP HEADERS

--- main.go
+++ main.go
@@ -15,14 +15,20 @@ func main() {
 
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "ENV VARS:")
+			fmt.Fprintln(w, "ENV VARS\n========")
 			for _, e := range os.Environ() {
 				fmt.Fprintln(w, e)
 			}
 			fmt.Fprintln(w)
+			fmt.Fprintln(w, "HTTP HEADERS\n============")
+			for k, v := range r.Header {
+				fmt.Fprintln(w, k, v)
+			}
+			fmt.Fprintln(w)
+
 			fmt.Fprintln(w, " -- Source of this service: https://github.com/wietsevenema/inspect --")
 		})
 
 	log.Println("Listening on port: " + port)
-	http.ListenAndServe(":"+port, nil)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
